72ac4dc7ea076240f35a8961dd2acb931a5d2ee8
eric.roys@gmail.com <eric.roys@gmail.com>
2024-09-10 08:23:31 -0500

    update mutation user create to work with tokening

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+mutation UserCreate($input: UserCreateInput) {
+  userCreate(input: $input) {
+   user {
+    email
+   }
+   token
+  }
+}
+`;
+
+export const ADD_BOOK = gql`
+  mutation UserSaveBook($email: String!, $input: SaveBookInput) {
+    userSaveBook(email: $email, input: $input) {
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation UserDeleteBook($email: String!, $bookId: String!) {
+    userDeleteBook(email: $email, bookId: $bookId) {
+      email
+      savedBooks {
+        title
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation UserLogin($input: LoginInput) {
+    userLogin(input: $input) {
+      email
+    }
+  }
+`;
