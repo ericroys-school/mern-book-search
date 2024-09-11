915cb94fc21502db69b4a1f9d01539e4ed642d84
eric.roys@gmail.com <eric.roys@gmail.com>
2024-09-11 18:51:09 -0500

    serve dist properly

--- server/server.js
+++ server/server.js
@@ -17,11 +17,17 @@ const startApolloServer = async () => {
   await server.start();
 
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  app.use(express.json()); 
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      console.log("****: " + __dirname)
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   }
 
   app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
